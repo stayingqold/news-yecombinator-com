69484e13f56b1658baffba0a177145aec2803b0f
stayingqold <finn@ucsd.edu>
2020-09-15 22:37:09 -0700

    fixed header

--- src/Header.js
+++ src/Header.js
@@ -10,14 +10,6 @@ const Header = () => {
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
         <div className="fw7 mr1">Ye News</div>
-        <a href="https://www.reddit.com/r/Kanye/new/" className="ml1 no-underline black">
-          new
-        </a>
-        <div className="ml1">|</div>
-        <a href="https://www.reddit.com/r/Kanye/top/?t=week" className="ml1 no-underline black">
-          top
-        </a>
-        <div className="ml1">|</div>
       </div>
     </div>
   );
