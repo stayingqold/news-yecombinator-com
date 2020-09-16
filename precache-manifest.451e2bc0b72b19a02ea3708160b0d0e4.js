4cc61f34d108f36fca42f1906ed69df7fbf46ded
stayingqold <finn@ucsd.edu>
2020-09-15 22:18:41 -0700

    Updates

---
+++ precache-manifest.451e2bc0b72b19a02ea3708160b0d0e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad7d3323967e799feb91ad80a3ee4fa",
+    "url": "/news-yecombinator-com/index.html"
+  },
+  {
+    "revision": "100976b443536983f2a4",
+    "url": "/news-yecombinator-com/static/css/main.f05f22ac.chunk.css"
+  },
+  {
+    "revision": "89d3b91449cd205015d9",
+    "url": "/news-yecombinator-com/static/js/2.8ddf5529.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/news-yecombinator-com/static/js/2.8ddf5529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100976b443536983f2a4",
+    "url": "/news-yecombinator-com/static/js/main.50601fe0.chunk.js"
+  },
+  {
+    "revision": "c4e39e7ef843a9b1d2ab",
+    "url": "/news-yecombinator-com/static/js/runtime-main.af174773.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news-yecombinator-com/static/media/logo.5d5d9eef.svg"
+  }
+]);
